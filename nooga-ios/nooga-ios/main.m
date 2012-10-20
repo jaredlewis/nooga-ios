32101c629d2423c825071e42de84fc038c430f93
Jared Lewis <jlewis@lamppostgroup.com>
2012-10-20 17:04:19 -0400

    * Initial Commit

---
+++ nooga-ios/nooga-ios/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  nooga-ios
+//
+//  Created by Jared Lewis on 10/20/12.
+//  Copyright (c) 2012 Jared Lewis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
